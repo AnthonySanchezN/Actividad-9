39e7943ff44e379571f111a18189d405dd8ec7d0
Anthony Sanchez <anthonysn2001@gmail.com>
2021-11-01 15:10:14 -0600

    Update a Act 10

--- src/deck2/Deck2.java
+++ src/deck2/Deck2.java
@@ -4,14 +4,14 @@
  */
 package deck2;
 
+import java.io.IOException;
 import java.util.*;
+import java.lang.*;
 
 public class Deck2 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    
+    public static void main(String[] args)throws IOException {
        Scanner sn = new Scanner(System.in);
         int opcion = 0;
         boolean salir = false;
@@ -28,45 +28,70 @@ public class Deck2 {
                 deck.add(card.palo[j]+ ", Rojo , " + card.valor[i]);
                 deck.add(card.palo[j]+ ", Negro , " + card.valor[i]);
         }
-        
+        }
         while(!salir){
             System.out.println("Bienvenido a Poker!");
             System.out.println("Selecciona una opcion: \n1. Mezclar deck. \n2. Sacar una carta. \n3. Carta al azar. \n4. Generar una mano de 5 cartas. \n0. Salir.");
             opcion = sn.nextInt();
-            
+            try{
             switch(opcion){
                 case 1:
                     System.out.println("Se mezclo el deck");
                     break;
                     
                 case 2:
+                    try{
                     System.out.println(deck.get(0));
                     deck.remove(0);
                     System.out.println("Quedan " + deck.size() + " cartas \n");
+                    }catch (IndexOutOfBoundsException e){
+                        System.out.println("No hay mas cartas.");
+                        System.exit(0);
+                    }
                     break;
                     
                 case 3: 
+                    try{
                     Random aleatorio = new Random();
                     System.out.println(deck.get(aleatorio.nextInt(deck.size())));
                     deck.remove(aleatorio.nextInt(deck.size()));
                     System.out.println("Quedan " + deck.size() + " cartas\n");
+                    }catch (IndexOutOfBoundsException e){
+                        System.out.println("No hay mas cartas");
+                        System.exit(0);
+                    }catch (IllegalArgumentException ex){
+                        System.out.println("No hay mas cartas!");
+                        System.exit(0);
+                    }
                     break;
                     
                 case 4: 
-                    for(int i = 0; i<4; i++){
+                    try{
+                    for(int i = 0; i<5; i++){
                         System.out.println(deck.get(i));
                     }
-                    for(int i = 0; i<4; i++){
+                    for(int i = 0; i<5; i++){
                         deck.remove(i);
                     }
                     System.out.println("Quedan " + deck.size() + " cartas");
+                    }catch (IndexOutOfBoundsException e){
+                        System.out.println("No hay mas cartas");
+                        System.exit(0);
+                    }
                     break;
                     
                 case 0:
                     salir = true;
                     break;
+                
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
             }
+            }catch (InputMismatchException e){
+                System.out.println("Opcion no valida");
+                }
         }
     }
-    
-}
+    }   
+
